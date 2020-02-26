3d7c6cfe7594c8d636fd54423aa0482d9abdc0b0
Marc <mcavigli@gmail.com>
2020-02-26 13:09:29 -0800

    Initial push for Task 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 	public Rigidbody rb;
 	public float speed = 500f;
-
-	// Start is called before the first frame update
-	void Start()
-	{
-
-	}
 
 	// Update is called once per frame
 	void FixedUpdate()
