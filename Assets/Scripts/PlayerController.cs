62d8e9680cabcc3cb9e4d94831cd7555ed94537c
Marc <mcavigli@gmail.com>
2020-02-27 12:25:44 -0800

    Initial push for Task 8 and 9

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -7,6 +8,16 @@ public class PlayerController : MonoBehaviour
 	public int health = 5;
 	private int score = 0;
 
+	void Update()
+	{
+		if (health == 0)
+		{
+			Debug.Log("Game Over!");
+			health = 5;
+			score = 0;
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
 	void FixedUpdate()
 	{
 		if (Input.GetKey("w"))
@@ -38,6 +49,10 @@ public class PlayerController : MonoBehaviour
 		{
 			health -= 1;
 			Debug.Log("Health: " + health);
+		}
+		if (other.tag == "Goal")
+		{
+			Debug.Log("You win!");
 		}
 	}
 }
